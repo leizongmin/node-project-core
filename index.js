9b56f4c56d53b848deead6bae517bf9ba52d13d1
Zongmin Lei <leizongmin@gmail.com>
2016-08-20 23:04:54 +0800

    fix: Cannot find module './dist/lib/method'

--- index.js
+++ index.js
@@ -8,6 +8,3 @@ module.exports = exports = require('./dist/lib/index').default;
 exports.ProjectCore = module.exports;
 
 exports.utils = require('./dist/lib/utils').default;
-
-exports.Method = require('./dist/lib/method').Method;
-exports.MethodManager = require('./dist/lib/method').MethodManager;
